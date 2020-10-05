6eb6c32c4689d596821651304b7e70cec756c6e9
Milos Zivkovic <miloszivkovic@jatheon.com>
2020-10-05 19:15:06 +0200

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module es-social-faker
+
+go 1.15
+
+require (
+	github.com/elastic/go-elasticsearch/v7 v7.5.1-0.20201001152130-296b8fc63d97
+	github.com/jaswdr/faker v1.0.4
+)
---
+++ main/faker.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v7/esutil"
+	"github.com/jaswdr/faker"
+	"log"
+	"math/rand"
+	"sync/atomic"
+	"time"
+)
+
+type Reaction struct {
+	Id          int32     `json:"id"`
+	PostId      int32     `json:"post_id"`
+	CreatedBy   string    `json:"created_by"`
+	Verb        string    `json:"verb"`
+	Reaction    string    `json:"reaction"`
+	CreatedDate time.Time `json:"created_date"`
+}
+
+func main() {
+	cfg := elasticsearch.Config{
+		Addresses: []string{
+			"http://log-store.default.192.168.1.103.xip.io/",
+		},
+	}
+
+	es, err := elasticsearch.NewClient(cfg)
+	if err != nil {
+		log.Fatalf("Error creating ES client: %s", err)
+	}
+
+	bi, err := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
+		Index:         "es-social",
+		Client:        es,
+		NumWorkers:    2000,
+		FlushBytes:    5e+6,
+		FlushInterval: 30 * time.Second,
+	})
+	if err != nil {
+		log.Fatalf("Error creating the indexer: %s", err)
+	}
+
+	f := faker.New()
+	rand.Seed(time.Now().UnixNano())
+	verbs := []string{"add", "update", "delete"}
+	posibReact := []string{"like", "love", "care", "haha", "wow", "sad", "angry"}
+	var reactions []Reaction
+	for i := 0; i < 100; i++ {
+		postId := rand.Int31()
+		for j := 0; j < 50; j++ {
+			name := f.Person().Name()
+			for k := 0; k < 10; k++ {
+				reactions = append(reactions, Reaction{
+					Id:          rand.Int31(),
+					PostId:      postId,
+					CreatedBy:   name,
+					Verb:        f.RandomStringElement(verbs),
+					Reaction:    f.RandomStringElement(posibReact),
+					CreatedDate: f.Time().TimeBetween(time.Now().AddDate(0, 0, -15), time.Now()),
+				})
+			}
+		}
+	}
+
+	var countSuccess uint64 = 0
+	for _, reaction := range reactions {
+
+		data, err := json.Marshal(reaction)
+		if err != nil {
+			log.Fatalf("Cannot encode reaction %d: %s", reaction.Id, err)
+		}
+
+		err = bi.Add(
+			context.Background(),
+			esutil.BulkIndexerItem{
+				Action: "index",
+				Body:   bytes.NewReader(data),
+				OnSuccess: func(ctx context.Context, item esutil.BulkIndexerItem, item2 esutil.BulkIndexerResponseItem) {
+					atomic.AddUint64(&countSuccess, 1)
+					println(countSuccess)
+				},
+			},
+		)
+		if err != nil {
+			log.Fatalf("Unexpected error: %s", err)
+		}
+	}
+
+	if err := bi.Close(context.Background()); err != nil {
+		log.Fatalf("Unexpected error: %s", err)
+	}
+
+}
